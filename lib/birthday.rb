95688ff851a3544202e05aab3ea072a09a2e56f9
Yu Hao (Tom) Lai <tomlai8882000@hotmail.com>
2020-07-03 21:31:34 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,8 +7,10 @@
 def happy_birthday(birthday_kids)
   # add your code snippet here!
   
-  birhtday_kids.each{|kids_name, age|puts "Happy Birthday #{kids_name}! You are now #{age} years old!"}
-  
-  birthday_kids
+  birhtday_kids.each do
+    |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+
   
 end
